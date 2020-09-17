f273b2e92a098a8ce9b4f30d6f4313e9548bce93
andresahumadaa <2298@holbertonschool.com>
2020-09-17 23:57:09 +0000

    C - Variables

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - abcdario
+ * Description: imprimir abcdario
+ * Return: 0
+ */
+int main(void)
+{
+int i;
+for (i = 97; i < 123; i++)
+{
+putchar(i);
+}
+putchar(10);
+return (0);
+}
---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - mayusculas en abc
+ * description: cambiar a mayusculas el abc
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+for (i = 97; i < 123; i++)
+{
+putchar(i);
+}
+for (i = 65; i < 91; i++)
+{
+putchar(i);
+}
+putchar(10);
+return (0);
+}
