b72a9d3428a701f902b0a75982e2702611bbbcfa
andresahumadaa <2298@holbertonschool.com>
2020-09-24 00:09:00 +0000

    Functions, nested loops

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * _abs - valor entero
+ * Description: valor absoluto de un entero
+ * Return: 0
+ */
+int _abs(int r)
+{
+if (r >= 0)
+{
+return (r);
+}
+else
+{
+return (-r);
+}
+}
