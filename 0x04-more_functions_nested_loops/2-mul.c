437cbf1d7a4ea8affd2adb8e29cd9e02507ee410
andresahumadaa <2298@holbertonschool.com>
2020-09-24 21:26:00 +0000

    More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * mul - multiplica a x b
+ * @a: primer numero
+ * @b: sgundo numero
+ * Description: aplicacion para multiplicar dos numeros
+ * Return: result(c)
+ */
+int mul(int a, int b)
+{
+int c;
+c = a * b;
+return (c);
+}
