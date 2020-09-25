58af23b4216e8452c2e02d371776da9a0533a0c4
andresahumadaa <2298@holbertonschool.com>
2020-09-25 02:20:10 +0000

    More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+/**
+ * print_triangle - triangulo
+ * @size: cantidad de veses
+ */
+void print_triangle(int size)
+{
+int f, c;
+f = 0;
+if (size > 0)
+{
+while (f < size)
+{
+c = 0;
+while (c < size)
+{
+if (c < size - f - 1)
+{
+_putchar(' ');
+}
+else
+{
+_putchar('#');
+}
+c++;
+}
+_putchar('\n');
+f++;
+}
+}
+if (size <= 0)
+{
+_putchar('\n');
+}
+}
