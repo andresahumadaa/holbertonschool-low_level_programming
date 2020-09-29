3c068ad0bbd4531a9579d77ea677de2514616570
andresahumadaa <2298@holbertonschool.com>
2020-09-29 18:28:40 +0000

    Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * puts_half - print the second half
+ * @str: parameter
+ */
+void puts_half(char *str)
+{
+int i, half;
+i = 0;
+while (*(str + i) != '\0')
+i++;
+if (i % 2 != 0)
+{
+i += 1;
+}
+for (half = i / 2; *(str + half) != '\0'; half++)
+{
+_putchar(*(str + half));
+}
+_putchar('\n');
+}
---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * _strcpy - copy string to dest
+ * @src: source string
+ * @dest: destined string of copy
+ * Return: copied string (dest)
+ */
+char *_strcpy(char *dest, char *src)
+{
+int i, n;
+i = 0;
+while (src[i])
+{
+i++;
+}
+for (n = 0; n <= i; n++)
+{
+dest[n] = src[n];
+}
+return (dest);
+}
