f86782ab82ced32d4777b9c2aa54297d6dd2c0ff
andresahumadaa <2298@holbertonschool.com>
2020-09-29 18:19:40 +0000

    Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * print_rev - imprimir alrevez
+ * @s: array
+ *
+ */
+void print_rev(char *s)
+{
+int i1, i2;
+i1 = 0;
+while (*(s + i1) != '\0')
+{
+i1++;
+}
+i2 = i1 - 1;
+while (i2 >= 0)
+{
+_putchar(*(s + i2));
+i2--;
+}
+_putchar('\n');
+}
