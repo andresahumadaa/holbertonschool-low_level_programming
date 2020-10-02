848a36858556de7c2623a420ce31d8164ad6a898
andresahumadaa <2298@holbertonschool.com>
2020-10-02 05:54:43 +0000

    More pointers, arrays and strings

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * reverse_array - reverse array of integers
+ * @n: elements
+ * @a: array
+ */
+void reverse_array(int *a, int n)
+{
+int i, t;
+i = 0;
+while (i < n - 1)
+{
+t = a[i];
+a[i] = a[n - 1];
+a[n - 1] = t;
+n--;
+i++;
+}
+}
