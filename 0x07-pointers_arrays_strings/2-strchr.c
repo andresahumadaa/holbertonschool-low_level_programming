4de3354f651eb40c183e4643add7fe256a8a6584
andresahumadaa <2298@holbertonschool.com>
2020-10-06 15:18:08 +0000

    pointers_arrays_strings

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * _strchr -locate a character ina a string
+ * @s: string pointer
+ * @c: character to find
+ * Return: t (Remaining) 0 (Not found)
+ */
+char *_strchr(char *s, char c)
+{
+int i = 0;
+for (; s[i] != '\0'; i++)
+{
+if (s[i] == c)
+{
+break;
+}
+}
+if (s[i] == c)
+return (&s[i]);
+return (0);
+}
