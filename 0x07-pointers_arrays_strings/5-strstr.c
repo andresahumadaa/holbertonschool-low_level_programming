b6ce1c549aead2e1131fe6763cb74ef3a5f30e7e
andresahumadaa <2298@holbertonschool.com>
2020-10-07 04:10:49 +0000

    pointers_arrays_strings

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * _strstr -locate a first ocurrence of substring
+ * @haystack: string
+ * @needle: substring
+ * Return: &haystack[i] (Success) 0 (Not found)
+ */
+char *_strstr(char *haystack, char *needle)
+{
+int i;
+for (i = 0; haystack[i] != '\0'; haystack++)
+{
+while (haystack[i] && needle[i] && haystack[i] == needle[i])
+{
+i++;
+}
+if (!needle[i])
+return (haystack);
+}
+return (0);
+}
