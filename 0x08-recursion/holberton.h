e505de088248555a63b445d2f5737d78ad9eea53
andresahumadaa <2298@holbertonschool.com>
2020-10-08 02:42:09 +0000

    recursion

---
+++ 0x08-recursion/holberton.h
@@ -0,0 +1,18 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+int _putchar(char c);
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+int is_palindrome(char *s);
+int factor(int n, int a);
+int prime(int n, int d);
+int length(char *s);
+int find_palindrome(char *s, int start, int end);
+
+#endif
