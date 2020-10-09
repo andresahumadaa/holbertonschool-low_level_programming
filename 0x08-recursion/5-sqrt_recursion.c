0fdfaf6990bbc8e7bc62b75c669d5bfa4f5edee2
andresahumadaa <2298@holbertonschool.com>
2020-10-09 02:49:14 +0000

    recursion

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+/**
+ * _sqrt_recursion - Square root of n
+ * @n: Entry value
+ * Return: result of n
+ */
+int _sqrt_recursion(int n)
+{
+if (n < 0)
+return (-1);
+if (n == 0 || n == 1)
+return (n);
+else
+return (factor(n, 2));
+}
+/**
+ * factor - find sqrt root factor
+ * @n: entry value
+ * @a: Sqrt root
+ * Return: a (Success) -1 (Not Found)
+ */
+int factor(int n, int a)
+{
+if ((a * a) == n)
+return (a);
+if ((a * a) > n)
+return (-1);
+else
+return (factor(n, a + 1));
+}
