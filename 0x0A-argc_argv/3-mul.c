22cbba610f8ad358f608c54dbd5eabf125ed106c
andresahumadaa <2298@holbertonschool.com>
2020-10-12 02:59:59 +0000

    C - argc, argv

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main -Entry point. Multiplication of two numbers
+ * @argc: argument counter
+ * @argv: argument vector
+ * Return: 0 (Success) 1 (Error)
+ */
+int main(int argc, char **argv)
+{
+unsigned int n1, n2;
+if (argc != 3)
+{
+printf("Error\n");
+return (1);
+}
+else
+{
+n1 = atoi(argv[1]);
+n2 = atoi(argv[2]);
+}
+printf("%d\n", n1 * n2);
+return (0);
+}
---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include "holberton.h"
+/**
+ * main - Entry point. Add numbers.
+ * @argc: argument counter
+ * @argv: argument vector
+ * Return: 0 (Success) 1 (Error)
+ */
+int main(int argc, char **argv)
+{
+int i, n;
+int sum = 0;
+if (argc < 2)
+{
+printf("0\n");
+return (0);
+}
+for (i = 1; i < argc; i++)
+{
+if (digit(argv[i]))
+{
+n = atoi(argv[i]);
+sum += n;
+}
+else
+{
+printf("Error\n");
+return (1);
+}
+}
+printf("%d\n", sum);
+return (0);
+}
+/**
+ * digit - find if character is digit
+ * @s: String pointer
+ * Return: 0 (Not found) 1 (Success)
+ */
+int digit(char *s)
+{
+int j;
+for (j = 0; s[j] != '\0'; j++)
+{
+if (!isdigit(s[j]))
+return (0);
+}
+return (1);
+}
