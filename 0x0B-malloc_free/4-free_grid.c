214a1d0985a464582e3656594dd32464f578087c
andresahumadaa <2298@holbertonschool.com>
2020-10-15 03:44:24 +0000

    C - malloc, free

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * free_grid - free a 2D dimansional grid
+ * @grid: pointer
+ * @height:  columns.
+ **/
+void free_grid(int **grid, int height)
+{
+int j;
+for (j = 0; j < height; j++)
+{
+free(grid[j]);
+}
+free(grid);
+}
