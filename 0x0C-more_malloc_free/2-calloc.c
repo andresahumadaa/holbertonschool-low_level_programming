9dd42512f069441e7b5a47dc242a4422f7a06790
andresahumadaa <2298@holbertonschool.com>
2020-10-16 02:22:58 +0000

    C - malloc, free

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * _calloc - allocate memory for an array
+ * @nmemb: elements
+ * @size: sizeof
+ * Return: p (Success) NULL (Failed)
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+char *p = NULL;
+unsigned int i = 0;
+if (!nmemb || !size)
+return (NULL);
+p = malloc(size * nmemb);
+if (!p)
+return (NULL);
+for (; i < (nmemb * size); i++)
+p[i] = 0;
+return (p);
+}
