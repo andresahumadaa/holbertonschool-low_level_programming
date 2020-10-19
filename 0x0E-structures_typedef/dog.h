e5d27e51a2a1ce1f7057c8681d99ada8e6d81c48
andresahumadaa <2298@holbertonschool.com>
2020-10-19 22:49:39 +0000

    C - Structures, typedef

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,24 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Define a new struct for dog
+ * @name: pointer to dog name
+ * @age: dog age
+ * @owner: dog owner's name
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+char *copys(char *s1, char *s2);
+
+#endif
