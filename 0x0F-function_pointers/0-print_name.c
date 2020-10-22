f8c42dd5d40fe63d217820e9672884380c57b242
andresahumadaa <2298@holbertonschool.com>
2020-10-22 03:28:01 +0000

    0x0F-function_pointers

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ * print_name - print a name
+ * @name: string to print
+ * @f: pointer to function print
+ */
+void print_name(char *name, void (*f)(char *))
+{
+if (name && f)
+f(name);
+}#include <stdio.h>
+#include "function_pointers.h"
+/**
+ * print_name - print a name
+ * @name: string to print
+ * @f: pointer to function print
+ */
+void print_name(char *name, void (*f)(char *))
+{
+if (name && f)
+f(name);
+}
