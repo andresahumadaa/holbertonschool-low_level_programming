43d7b0a93d6d78f6d857395b024a92a249fd74f1
andresahumadaa <2298@holbertonschool.com>
2020-11-05 03:13:04 +0000

    0x13-more_singly_linked_lists

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * free_listint2 - free a listint list
+ * @head: pointer to head pointer
+ */
+void free_listint2(listint_t **head)
+{
+listint_t *Current_Node;
+if (head == NULL)
+return;
+while (*head)
+{
+Current_Node = *head;
+*head = Current_Node->next;
+free(Current_Node);
+}
+}
