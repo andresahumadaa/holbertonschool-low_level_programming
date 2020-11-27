7f31099c2f4875c557aee6aeb2061a2930ce1796
andresahumadaa <2298@holbertonschool.com>
2020-11-27 23:14:59 +0000

    correciones

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - entry point first fibonacci numbers
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+long int t, n1, n2, n3, sum;
+n1 = 0;
+n2 = 1;
+sum = 0;
+for (t = 0; t < 50; t++)
+{
+n3 = n1 + n2;
+if (n3 % 2 == 0 && n3 < 4000000)
+{
+sum += n3;
+}
+n1 = n2;
+n2 = n3;
+}
+printf("%ld\n", sum);
+return (0);
+}
