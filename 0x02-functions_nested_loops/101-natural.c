32c0a4faaa943ed86928ec28974c1378c9a38867
andresahumadaa <2298@holbertonschool.com>
2020-11-27 23:11:13 +0000

    correciones

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: sucess 0
+ */
+int main(void)
+{
+int x, sum;
+sum = 0;
+for (x = 0; x < 1024; x++)
+{
+if ((x % 3) == 0 || (x % 5) == 0)
+{
+sum += x;
+}
+}
+printf("%d\n", sum);
+return (0);
+}
---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - entry point first fibonacci numbers
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+long int t, n1, n2, n3;
+n1 = 0;
+n2 = 1;
+for (t = 0; t < 50; t++)
+{
+n3 = n1 + n2;
+if (t != 49)
+{
+printf("%ld, ", n3);
+}
+else
+{
+printf("%ld", n3);
+}
+n1 = n2;
+n2 = n3;
+}
+printf("\n");
+return (0);
+}
