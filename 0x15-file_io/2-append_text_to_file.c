3d252c6d30e609632d0242ba1d92cf79975c3661
andresahumadaa <2298@holbertonschool.com>
2020-11-27 21:16:58 +0000

    correciones

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,45 +1,30 @@
 #include "holberton.h"
 /**
- * append_text_to_file - appends text at the end of a file
- * @filename: name of the file
- * @text_content: text to append at the end of the file
- * Return: 1 (Success) -1 (Failed)
- */
-int _strlen(char *s)
-{
-int len = 0;
-while (*s != '\0')
-{
-len++;
-s++;
-}
-return (len);
-}
-/**
- *append_text_to_file - a function that appends text at the end of a file
- *@filename: is the name of the file to create
- *@text_content: s a NULL terminated string to write to the file
- *Return: 1 on success, -1 on failure
+ * append_text_to_file - function appends a text at the end of a file.
+ * @filename: Name of the file to append a text.
+ * @text_content: is a NULL terminated string to append to the file.
+ *
+ * Return: 1 on success, -1 on failure.
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-int fd, write_char;
-if (filename == NULL)
-return (-1);
+int fd, i, validate;
+if (text_content == NULL)
+text_content = "";
+for (i = 0; text_content[i]; i++)
+{}
+if (filename)
+{
 fd = open(filename, O_WRONLY | O_APPEND);
 if (fd == -1)
-return (-1);
-if (text_content == NULL)
 {
+return (-1);
+}
+validate = write(fd, text_content, i);
+if (validate == -1)
+return (-1);
 close(fd);
 return (1);
 }
-write_char = write(fd, text_content, _strlen(text_content));
-if (write_char == -1)
-{
-close(fd);
 return (-1);
-}
-close(fd);
-return (1);
 }
