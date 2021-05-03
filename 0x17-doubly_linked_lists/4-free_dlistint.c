8fa9b2c87f7eb940b832a1465982e50deb03bcd0
andresahumadaa <2268@holbertonschool.com>
2021-05-03 03:02:52 +0000

    free_dlistint

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * free_dlistint - free a dlistint_t list.
+ * @head: Head of the list
+ * Return: void
+ */
+void free_dlistint(dlistint_t *head)
+{
+if (!head)
+{
+return;
+}
+if (head->next)
+{
+free_dlistint(head->next);
+}
+free(head);
+}
